4421b6975c9c1f6f6a0d0eb4a69c56f58884519d
Christian Lascsak <christian.lascsak@anecon.com>
2022-03-12 14:55:45 +0100

    fix cwd of build task

--- Task01/src/main.cpp
+++ Task01/src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char const* argv[]) {
-    printf("Rotate a vector around an axis...");
+    auto x = 5;
+    auto c = "helloboi";
+    printf("Rotate a vector around an axis... %i %s", x, c);
     return 0;
 }
