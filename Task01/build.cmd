45bc700c46b5a2d77862829c0d32dab8becef57c
Christian Lascsak <christian.lascsak@anecon.com>
2022-03-20 10:43:23 +0100

    update tasks.json to use build cmd for build and ignore pdb file

--- Task01/build.cmd
+++ Task01/build.cmd
@@ -1,1 +1,2 @@
-cl /Zi /EHsc /Fe: out\rotation.exe main.cpp
+@REM  see: https://docs.microsoft.com/en-us/cpp/build/reference/compiler-options?view=msvc-170
+cl /Zi /EHsc /Fe: out\rotation.exe main.cpp /Fo"out\rotation.obj"
