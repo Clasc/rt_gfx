d900ae91c8dcbe5214fa1fa2885542ae1679e294
Christian Lascsak <christian.lascsak@anecon.com>
2022-03-20 10:06:30 +0100

    Do some refactoring and logging

--- Task01/main.cpp
+++ Task01/main.cpp
@@ -7,8 +7,21 @@
 #include "./lib/glm/glm/ext.hpp"
 #include "./lib/glm/glm/gtc/matrix_transform.hpp"
 #include "./lib/glm/glm/gtx/hash.hpp" 
+#include <vector>
 
 namespace task {
+    std::vector<glm::vec3> create_vecs() {
+        return std::vector<glm::vec3>{
+            glm::vec3(1.0f, 0.0f, 0.0f),
+                glm::vec3(0.0f, 2.0f, 0.0f),
+                glm::vec3(0.0f, 0.0f, 4.0f)
+        };
+    }
+
+    void print_seperator() {
+        std::cout << "__________________" << std::endl;
+    }
+
     glm::mat4 create_rotation_mat(const glm::vec3 axis, const float angle) {
         // create a rotation matrix from the given vector
         auto normalized = glm::normalize(axis);
@@ -17,8 +30,8 @@ namespace task {
         return glm::rotate(model, angle, normalized);
     }
 
-    void print_vector(glm::vec3 vec) {
-        std::cout << glm::to_string(vec) << std::endl;
+    void print_vector(const char* name, glm::vec3 vec) {
+        std::cout << name << " :" << glm::to_string(vec) << std::endl;
     }
 
     glm::vec3 rotate_around(const glm::vec3 source, const glm::vec3 axis, const float angle) {
@@ -28,13 +41,27 @@ namespace task {
         return rotation_matrix * glm::vec4(source, 0.0f);
     }
 
+    void rotate_all(const glm::vec3 axis, const float angle) {
+        auto vecs = create_vecs();
+        for (const auto v : vecs) {
+            print_vector("origin", v);
+            auto result = rotate_around(v, axis, angle);
+            print_vector("result", result);
+            print_seperator();
+        }
+    }
+
+    void print_angle_axis(const glm::vec3 axis, const float angle) {
+        std::cout << "angle: " << angle << std::endl;
+        print_vector("axis", axis);
+    }
 }
 
 int main(int argc, char const* argv[]) {
     auto const angle = glm::radians(90.0f);
     auto rotation_vector = glm::vec3(0.0f, 0.0f, 1.0f);
-    auto origin = glm::vec3(0.0f, 1.0f, 0.0f);
-    auto result = task::rotate_around(origin, rotation_vector, angle);
-    task::print_vector(result);
+    task::print_angle_axis(rotation_vector, angle);
+    std::cout << "--- rotate vectors --- " << std::endl << std::endl;
+    task::rotate_all(rotation_vector, angle);
     return 0;
 }
