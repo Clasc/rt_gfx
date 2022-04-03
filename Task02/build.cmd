bb1e27cf2f3f9780ac813b9eb5ae2ae993e287db
Christian Lascsak <christian.lascsak@anecon.com>
2022-04-03 14:18:05 +0200

    Task02: init project

---
+++ Task02/build.cmd
@@ -0,0 +1,2 @@
+@REM  see: https://docs.microsoft.com/en-us/cpp/build/reference/compiler-options?view=msvc-170
+cl /Zi /EHsc /Fe: out\task02.exe main.cpp /Fo"out\task02.obj"
---
+++ Task02/main.cpp
@@ -0,0 +1,15 @@
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+#include <stdio.h>
+#include <iostream>
+#include "./lib/glm/glm/glm.hpp"
+#include "./lib/glm/glm/ext.hpp"
+#include "./lib/glm/glm/gtc/matrix_transform.hpp"
+#include "./lib/glm/glm/gtx/hash.hpp" 
+#include <vector>
+
+int main(int argc, char const* argv[]) {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
